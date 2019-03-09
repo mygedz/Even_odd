4e1fbf65258a5d1039faa29476183cb9958115f7
Alexandr Gedz <mygedz@gmail.com>
2019-03-09 03:00:05 +0300

    Релиз

---
+++ Четное не четное/Form1.Designer.cs
@@ -0,0 +1,103 @@
+﻿namespace Четное_не_четное
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.verify = new System.Windows.Forms.Button();
+            this.Clear = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(152, 86);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Enter += new System.EventHandler(this.textBox1_Enter);
+            this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
+            // 
+            // verify
+            // 
+            this.verify.Location = new System.Drawing.Point(11, 120);
+            this.verify.Name = "verify";
+            this.verify.Size = new System.Drawing.Size(153, 36);
+            this.verify.TabIndex = 1;
+            this.verify.Text = "Проверить";
+            this.verify.UseVisualStyleBackColor = true;
+            this.verify.Click += new System.EventHandler(this.verify_Click);
+            // 
+            // Clear
+            // 
+            this.Clear.Location = new System.Drawing.Point(11, 162);
+            this.Clear.Name = "Clear";
+            this.Clear.Size = new System.Drawing.Size(153, 36);
+            this.Clear.TabIndex = 2;
+            this.Clear.Text = "Очистить";
+            this.Clear.UseVisualStyleBackColor = true;
+            this.Clear.Click += new System.EventHandler(this.Clear_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(32, 101);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(107, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Вы ничего не ввели";
+            this.label1.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(177, 206);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Clear);
+            this.Controls.Add(this.verify);
+            this.Controls.Add(this.textBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Чет не чет";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button verify;
+        private System.Windows.Forms.Button Clear;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Четное не четное/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Четное_не_четное
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Clear_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+        }
+
+        private void verify_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "")
+            {
+                label1.Visible = true;
+                textBox1.BackColor = Color.LightCoral;
+            }
+            else
+            {
+                byte value = Convert.ToByte(textBox1.Text[textBox1.Text.Length - 1]);
+                if (value % 2 == 0)
+                {
+                    MessageBox.Show("Четное");
+                }
+                else
+                {
+                    MessageBox.Show("Не четное");
+                }
+            }
+        }
+
+        private void textBox1_Enter(object sender, EventArgs e)
+        {            
+            label1.Visible = false;
+            textBox1.BackColor = Color.White;
+        }
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            char number = e.KeyChar;
+            if (!Char.IsDigit(number) && number != 8)
+            {
+                e.Handled = true;
+            }
+        }
+    }
+}
